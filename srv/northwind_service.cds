41cfccb5d7a4927099061cc44f7358123d60b128
Milton Chandradas <milton.chandradas@sap.com>
2021-11-30 03:33:52 +0000

    emitting and consuming events...

--- srv/northwind_service.cds
+++ srv/northwind_service.cds
@@ -5,4 +5,9 @@ service NorthwindService {
     entity Products as projection on Northwind.Products {
         key ProductID as id, ProductName as name, UnitPrice as unitPrice, UnitsInStock as stock, Discontinued as discontinued
     };
+
+    event feedback: {
+        rating: Integer;
+        review: String;
+    }
 }
--- srv/northwind_service.js
+++ srv/northwind_service.js
@@ -8,4 +8,19 @@ module.exports = async (srv) => {
     const db = externalService.tx(req);
     return db.run(req.query);
   });
+
+  srv.after("READ", Products, (req) => {
+    srv.emit("feedback", {
+      rating: 5,
+      review: "Amazing product... ",
+    });
+    console.log("Event raised: feedback");
+  });
+
+  srv.on("feedback", payload => {
+      const { rating, review } = payload.data;
+      console.log("Rating: ", rating);
+      console.log("Review: ", review);
+
+  })
 };
